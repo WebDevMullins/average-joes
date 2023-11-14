cd14365edfc426304e948fc8d774b3b58d8fc122
tgaytan <tgaytan14@utexas.edu>
2023-11-13 20:40:09 -0600

    added seed data and models/index.js file

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,23 @@
 const sequelize = require('../config/connection')
 const seedUsers = require('./user-seeds')
 const seedTrainers = require('./trainer-seeds');
+const seedMemberShipPlan = require('./membershipPlan-seeds');
+const seedTier = require('./membershipTier-seeds');
 
 const seedDatabase = async () => {
 	await sequelize.sync({ force: true })
 	console.log('\n----- DATABASE SYNCED -----\n')
 
-	await seedUsers()
-	console.log('\n----- USERS SEEDED -----\n')
-
 	await seedTrainers()
+	console.log('\n----- TRAINERS SEEDED -----\n')
+
+	await seedMemberShipPlan()
+	console.log('\n----- PLANS SEEDED -----\n')
+
+	await seedTier()
+	console.log('\n----- TIERS SEEDED -----\n')
+
+	await seedUsers()
 	console.log('\n----- USERS SEEDED -----\n')
 
 	process.exit(0)
