cbcbf6f1b31cdfe82e77f2eda7cad503af44c593
tgaytan <tgaytan14@utexas.edu>
2023-11-13 19:49:16 -0600

    added seed data for Trainers. added new columns to user-seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,15 @@
 const sequelize = require('../config/connection')
 const seedUsers = require('./user-seeds')
+const seedTrainers = require('./trainer-seeds');
 
 const seedDatabase = async () => {
 	await sequelize.sync({ force: true })
 	console.log('\n----- DATABASE SYNCED -----\n')
 
 	await seedUsers()
+	console.log('\n----- USERS SEEDED -----\n')
+
+	await seedTrainers()
 	console.log('\n----- USERS SEEDED -----\n')
 
 	process.exit(0)
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,27 +4,37 @@ const userData = [
 	{
 		name: 'John Doe',
 		email: 'john@example.com',
-		password: 'password123'
+		password: 'password123',
+		membership_tier_id: 1,
+		membership_plan_id: 1
 	},
 	{
 		name: 'Jane Doe',
 		email: 'jane@example.com',
-		password: 'securepass'
+		password: 'securepass',
+		membership_tier_id: 1,
+		membership_plan_id: 1
 	},
 	{
 		name: 'Bob Smith',
 		email: 'bob@example.com',
-		password: 'bobspassword'
+		password: 'bobspassword',
+		membership_tier_id: 1,
+		membership_plan_id: 1
 	},
 	{
 		name: 'Alice Johnson',
 		email: 'alice@example.com',
-		password: 'alicepass'
+		password: 'alicepass',
+		membership_tier_id: 1,
+		membership_plan_id: 1
 	},
 	{
 		name: 'Charlie Brown',
 		email: 'charlie@example.com',
-		password: 'charliepass'
+		password: 'charliepass',
+		membership_tier_id: 1,
+		membership_plan_id: 1
 	}
 ]
 
