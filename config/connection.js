a2ad623e18be042d1c1b5dd836da9872990391ca
tgaytan <tgaytan14@utexas.edu>
2023-11-13 19:17:58 -0600

    reverted change in connection.js file

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-	host: '127.0.0.1',
+	host: 'localhost',
 	dialect: 'mysql',
 	port: 3306
 })
