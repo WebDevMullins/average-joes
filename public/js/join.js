6ee2f2a284fd80eb6767b5e4f00a1763f27426ff
clayg <claytondguerrero@gmail.com>
2023-11-14 19:24:26 -0600

    join.js is now running but not working properly

--- public/js/join.js
+++ public/js/join.js
@@ -1,25 +1,25 @@
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const firstName = document.querySelector("#firstName").value.trim();
-  const lastName = document.querySelector("#lastName").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  // const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
 
-  if (firstName && lastName && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ firstName, lastName, email, password }),
-      headers: { "Content-Type": "application/json" },
+  if ( email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({  email, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace('/');
     } else {
-      alert("Failed to join.");
+      alert('Failed to sign up.');
     }
   }
 };
+
 
 document
   .querySelector(".signup-form")
--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,24 @@
 const loginFormHandler = async (e) => {
-	e.preventDefault()
+  e.preventDefault();
 
-	const email = document.querySelector('#email').value.trim()
-	const password = document.querySelector('#password').value.trim()
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
 
-	if (email && password) {
-		const response = await fetch('/api/users/login', {
-			method: 'POST',
-			body: JSON.stringify({ email, password }),
-			headers: { 'Content-Type': 'application/json' }
-		})
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-		if (response.ok) {
-			document.location.replace('/')
-		} else {
-			alert('Invalid login credentials, please try again.')
-		}
-	}
-}
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Invalid login credentials, please try again.");
+    }
+  }
+};
 
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler)
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
