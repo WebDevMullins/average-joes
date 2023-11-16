cf67e172f27ed476daea7ca0d998aba2e42de020
tgaytan <tgaytan14@utexas.edu>
2023-11-15 20:24:05 -0600

    added background image for price plans

--- public/js/fetchServices.js
+++ public/js/fetchServices.js
@@ -1,13 +1,6 @@
-// var requestURL = 'api/membership/tiers';
-var requestURL = 'http://localhost:3001/api/membership/tiers';
-
 const init = async () => {
-    const response = await fetch(requestURL)
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // });
-    console.log(response);
+    const response = await fetch('/api/membership/tiers');
+    return response;
 };
 
 init();
