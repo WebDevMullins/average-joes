cb97cc91c2de968f6d9f5d97a22878f18dca07f3
WebDevMullins <webdevmullins@gmail.com>
2023-11-16 17:07:12 -0600

    add membership page, js, and route

--- controllers/api/plansTiersRoutes.js
+++ controllers/api/plansTiersRoutes.js
@@ -1,28 +1,48 @@
-const router = require('express').Router();
-const { MembershipTier, MembershipPlan, Trainer } = require('../../models');
+const router = require('express').Router()
+const { MembershipTier, MembershipPlan, Trainer, User } = require('../../models')
 
 router.get('/tiers', async (req, res) => {
-    try {
-        const tierData = await MembershipTier.findAll({
-            include: [{
-                model: Trainer
-            }]
-        });
-        const tier = tierData.map((tier) => tier.get({ plain: true }));
-        res.render('tier', { tier} );
-        // res.status(200).json(tierData);
-    } catch(err) {
-        res.status(400).json(err);
-    };
-});
+	try {
+		const tierData = await MembershipTier.findAll({
+			include: [
+				{
+					model: Trainer
+				}
+			]
+		})
+		const tier = tierData.map((tier) => tier.get({ plain: true }))
+		res.render('tier', { tier })
+		// res.status(200).json(tierData);
+	} catch (err) {
+		res.status(400).json(err)
+	}
+})
 
 router.get('/plans', async (req, res) => {
-    try {
-        const planData = await MembershipPlan.findAll();
-        res.status(200).json(planData);
-    } catch(err) {
-        res.status(400).json(err);
-    };
-});
+	try {
+		const planData = await MembershipPlan.findAll()
+		res.status(200).json(planData)
+	} catch (err) {
+		res.status(400).json(err)
+	}
+})
 
-module.exports = router;
+router.get('/', async (req, res) => {
+	try {
+		// Get all membership plans and JOIN with tiers
+		const membershipData = await MembershipPlan.findAll({
+			attributes: ['name', 'duration'],
+			include: [{ MembershipTier, attributes: ['name'] }]
+		})
+		// Serialize data
+		const membership = membershipData.map((membership) => membership.get({ plain: true }))
+		// Render template with membership data
+		res.render('membership', { membership })
+		// res.json(membership)
+	} catch (err) {
+		// Handle server error
+		res.status(500).json(err)
+	}
+})
+
+module.exports = router
